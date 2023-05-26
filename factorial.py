6435dc83a59a8bfa28bc277432af3f5409c5c716
InsaafM <insaaf.saheed@gmail.com>
2023-05-25 20:03:32 -0400

    Added python script

---
+++ factorial.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Get user input
+num = int(input("Enter a number: "))
+
+# Calculate factorial
+result = factorial(num)
+
+# Print the result
+print("The factorial of", num, "is", result)
